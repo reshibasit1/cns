43fb799ce49a7563512a416a640c60bba13cfafe
reshibasit1 <88622569+reshibasit1@users.noreply.github.com>
2022-08-03 10:50:44 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -32,51 +32,33 @@ return 0;
 
 
 Output
-
-Enter a message to decrypt: ebdh
-Enter key: 4
-Decrypted message: axzd
-
-
-Program for Caesar Cipher in C++
-Encryption
-#include<iostream>
-
-using namespace std;
-
+#include<stdio.h>
+ 
 int main()
 {
-	char message[100], ch;
-	int i, key;
-	
-	cout << "Enter a message to encrypt: ";
-	cin.getline(message, 100);
-	cout << "Enter key: ";
-	cin >> key;
-	
-	for(i = 0; message[i] != '\0'; ++i){
-		ch = message[i];
-		
-		if(ch >= 'a' && ch <= 'z'){
-			ch = ch + key;
-			
-			if(ch > 'z'){
-				ch = ch - 'z' + 'a' - 1;
-			}
-			
-			message[i] = ch;
-		}
-		else if(ch >= 'A' && ch <= 'Z'){
-			ch = ch + key;
-			
-			if(ch > 'Z'){
-				ch = ch - 'Z' + 'A' - 1;
-			}
-			
-			message[i] = ch;
-		}
-	}
-	
-	cout << "Encrypted message: " << message;
-	
-	return 0;
+char message[100], ch;
+int i, key;
+printf("Enter a message to decrypt: ");
+gets(message);
+printf("Enter key: ");
+scanf("%d", &key);
+for(i = 0; message[i] != '\0'; ++i){
+ch = message[i];
+if(ch >= 'a' && ch <= 'z'){
+ch = ch - key;
+if(ch < 'a'){
+ch = ch + 'z' - 'a' + 1;
+}
+message[i] = ch;
+}
+else if(ch >= 'A' && ch <= 'Z'){
+ch = ch - key;
+if(ch < 'A'){
+ch = ch + 'Z' - 'A' + 1;
+}
+message[i] = ch;
+}
+}
+printf("Decrypted message: %s", message);
+return 0;
+};
